8ae3f3d1526f2b64111ffc874ca8912627bb813d
Anuska86 <anasrm86@gmail.com>
2024-06-13 15:55:01 +0200

    Mongo

--- README.md
+++ README.md
@@ -8,9 +8,13 @@ install nodemon npm install --save-dev nodemon
 
 install morgan npm install morgan
 
+moongose npm install mongoose
+
 start: node index.js or npm start
 
 start nodemon npm run dev
+
+Mongo: node mongo.js 
 
 LINK https://helsinkipart3-backend.onrender.com/
 
---
+++ mongo.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+var config = require('./config.json');
+console.log(config);
+
+const url = `mongodb+srv://${config.MONGO_USER}:${config.MONGO_PASSWORD}@cluster0.sw7wgdz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const phoneSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  important: Boolean,
+});
+
+const Phone = mongoose.model("Phone", phoneSchema);
+
+
+const phone = new Phone({
+    name: "Arto Hellas",
+    number: "040-123456",
+    important:true
+});
+
+phone.save().then((result) => {
+    console.log("phone saved!");
+  });
+
+Phone.find({}).then(result => {
+    console.log('Init:')
+    result.forEach(phone => {
+      console.log('Tag:' +phone)
+    })
+    mongoose.connection.close()
+  })
